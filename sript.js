7840e916c286acdadb66f945cff6506fbc7403ec
David <107300355+lordryns@users.noreply.github.com>
2025-02-15 17:12:00 +0100

    Add files via upload

--- sript.js
+++ sript.js
@@ -1,15 +1,20 @@
-import { Client, Databases } from "appwrite";
+// import { Client, Databases } from "appwrite";
 
-const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject('<YOUR_PROJECT_ID>'); // Your project ID
+// const client = new Client()
+//    .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+//    .setProject('<YOUR_PROJECT_ID>'); // Your project ID
 
-const databases = new Databases(client);
+//const databases = new Databases(client);
 
-const result = await databases.listDocuments(
-    '<DATABASE_ID>', // databaseId
-    '<COLLECTION_ID>', // collectionId
-    [] // queries (optional)
-);
+// const result = await databases.listDocuments(
+//    '<DATABASE_ID>', // databaseId
+  //  '<COLLECTION_ID>', // collectionId
+    //[] // queries (optional)
+//);
 
-console.log(result);
+//console.log(result);
+
+const urlParams = new URLSearchParams(window.location.search);
+const telegramData = Object.fromEntries(urlParams.entries());
+
+console.log(telegramData); // Contains id, username, auth_date, hash, etc.
